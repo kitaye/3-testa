a9e9b8303e2f548c3167e8ae91606a1c63f65f45
Natalya <natalyamyunster@yahoo.com>
2019-09-06 15:14:19 +0200

    Changed function in js file

--- script.js
+++ script.js
@@ -1,39 +1,81 @@
-document.addEventListener('click', (event) => catchClick(event));
+//Buttons Tests with description in main block
+const btnIQ = document.querySelector(".main.container.iq");
+const btnIA = document.querySelector(".main.container.ia");
+const btnMBTI = document.querySelector(".main.container.mbti");
+const btnSMIL= document.querySelector(".main.container.smil");
 
 
+//Main block
+const main = document.querySelector("main");
 
-function catchClick(event) {
-    const clickedElement = event.target;
-    hideMain();
-    switch(clickedElement.innerHTML.trim()) {
-        case 'TEST IQ':
-            showTest(document.querySelector('.iq_test'));
-            break;
-        case 'TEST PAEI (Izchak Adizes)':
-            showTest(document.querySelector('.ia_test'));
-            break;
-        case 'TEST MBTI':
-            showTest(document.querySelector('.mbti_test'));
-            break;
-        case 'TEST MMPI(СМИЛ)':
-            showTest(document.querySelector('.smil_test'));
-            break;
-    }
+//Tests section
+const testBodyIQ = document.querySelector(".iq_test");
+const testBodyIA = document.querySelector(".ia_test");
+const testBodyMBTI = document.querySelector(".mbti_test");
+const testBodySMIL = document.querySelector(".smil_test");
 
+console.log(testBodyMBTI);
+console.log(testBodySMIL);
+
+showTest(btnIQ, main, testBodyIQ);
+showTest(btnIA, main, testBodyIA);
+showTest(btnMBTI, main, testBodyMBTI);
+showTest(btnSMIL, main, testBodySMIL);
+
+
+function showTest(button, elementToHide, elementToShow) {
+
+    button.addEventListener("click", function() {
+        elementToHide.style.display = "none";
+        elementToShow.style.display = "block";
+    });
 }
 
-function showTest(test){
-    test.style.display = "block";
-}
 
-function hideMain() {
-    let el = document.querySelector('main');
 
-    // if(el.style.display !== 'none') {
-        el.style.display = 'none';
-    // }
-    // else {
-    //     el.style.display = 'block';
-    // }
-}
+
+
+
+
+
+// document.addEventListener('click', (event) => catchClick(event));
+//
+//
+//
+// function catchClick(event) {
+//     const clickedElement = event.target;
+//     console.log(clickedElement);
+//     hideMain();
+//     switch(clickedElement.innerHTML.trim()) {
+//         case 'TEST IQ':
+//             showTest(document.querySelector(' .iq_test'));
+//             break;
+//         case 'TEST PAEI (Izchak Adizes)':
+//             showTest(document.querySelector('.ia_test'));
+//             break;
+//         case 'TEST MBTI':
+//             showTest(document.querySelector('.mbti_test'));
+//             break;
+//         case 'TEST MMPI(СМИЛ)':
+//             showTest(document.querySelector('.smil_test'));
+//             break;
+//     }
+//
+// }
+//
+//
+// function showTest(test){
+//     test.style.display = "block";
+// }
+//
+// function hideMain() {
+//     let el = document.querySelector('main');
+//
+//     // if(el.style.display !== 'none') {
+//         el.style.display = 'none';
+//     // }
+//     // else {
+//     //     el.style.display = 'block';
+//     // }
+// }
 
