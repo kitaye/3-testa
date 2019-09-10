9e0f2f8693abb6a65726a319d8c88d89652d5614
WhitcherX-PC <kot_X@mail.ru>
2019-09-10 19:40:30 +1000

    вместо main page теперь test lit. Обновил положение файлов по смил и мбти тестам в проекте

--- scripts/smil_script.js
+++ scripts/smil_script.js
@@ -106,16 +106,6 @@ function formResult() {
 }
 
 function readQuestionList() {
-	let questionsList = ['I always tell only the truth.',
-	                 'My affairs are worse than others.',
-	                 'I will not begin to tell everything about myself to even a loved one.',
-	                 'I am a completely healthy person.',
-	                 'I often have a sad mood.',
-	                 'I do not care if the people around me like me.',
-	                 'I strive to succeed in life.',
-	                 'My character is too soft.',
-	                 'I strive to defend my opinion at all costs.',
-	                 'Often I worry about nothing.']
 	return questionsList;
 }
 
